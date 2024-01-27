c70e090175644ba27f3a69cf6b42101941f1b5eb
nirman2004 <nd2719@ece.jgec.ac.in>
2024-01-27 14:00:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function show_result(){
+ let m = document.querySelector("#m").value;
+ let v = document.querySelector("#v").value;
+ let r = document.querySelector("#r").value;
+
+ let acc =  parseFloat(v) * parseFloat(v) /parseFloat(r);
+
+ document.querySelector(".acc").innerHTML = acc;
+
+ let f =  parseFloat(m) * parseFloat(v) * parseFloat(v) / parseFloat(r);
+
+ document.querySelector(".f").innerHTML = f;
+}
+
